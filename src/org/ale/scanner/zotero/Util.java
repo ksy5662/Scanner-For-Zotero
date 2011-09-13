0e5a45afb42e23f5b61c8ee4a9354dc52b064b78
John M. Schanck <john@anomos.info>
2011-09-13 19:01:50 -0400

    Util: ISBN-10/13 checksum and validation

--- src/org/ale/scanner/zotero/Util.java
+++ src/org/ale/scanner/zotero/Util.java
@@ -49,18 +49,77 @@ public class Util {
     
     public static int parseCODE128(String content){
         // A CODE_128 result may be an ISBN-10
-        if(content.length() == 10 && TextUtils.isDigitsOnly(content)){
+        if(isValidISBN(content)){
             return SCAN_PARSE_ISBN;
         }
         return SCAN_PARSE_FAILURE;
     }
 
+    public static int checksumISBN13(String isbn){
+        // The check digit of an ISBN-13 is k such that
+        // the sum of the even digits plus three times the sum
+        // of the odd digits equals -k mod 10
+        int e = (isbn.charAt(0) + isbn.charAt(2) + isbn.charAt(4) +
+                 isbn.charAt(6) + isbn.charAt(8) + isbn.charAt(10)) - 6*'0'; 
+        int o = (isbn.charAt(1) + isbn.charAt(3) + isbn.charAt(5) +
+                 isbn.charAt(7) + isbn.charAt(9) + isbn.charAt(11)) - 6*'0';
+        return (10 - ((e + 3*o) % 10)) % 10;
+    }
+
+    public static int checksumISBN10(String isbn){
+        // The check digit of an ISBN-10 is k such that
+        // \sum_{i=0}^{8}{isbn[i]*(10-i)} + k = 0 mod 11
+        int r1 = 0;
+        int r2 = 0;
+        for(int i=0; i<=8; i++){
+            r1 += (int) isbn.charAt(i) - '0';
+            r2 += r1;
+        }
+        r2 += r1;
+        return (11 - (r2 % 11)) % 11;
+    }
+
+    public static boolean isValidISBN(String isbn){
+        if(isbn.length() == 10){ // ISBN-10
+            // Last character can be 'X' to denote the value 10
+            if(!TextUtils.isDigitsOnly(isbn.substring(0, 8)))
+                return false;
+
+            int checksum = checksumISBN10(isbn);
+            char expected = (checksum == 10) ? 'X' : Character.forDigit(checksum, 10);
+
+            char given = isbn.charAt(9);
+            if(given == 'x') given = 'X'; // Just in case..
+
+            // Validate against the expected checksum
+            if(given != expected)
+                return false;
+        }else if(isbn.length() == 13){ // ISBN-13
+            String ean = isbn.substring(0, 3);
+            if(!(ean.equals("978") || ean.equals("979")))
+                return false;
+
+            int checksum = checksumISBN13(isbn);
+            char expected = Character.forDigit(checksum, 10);
+
+            // Validate against the expected checksum
+            if(isbn.charAt(12) != expected)
+                return false;
+        }else{ // not an ISBN
+            return false;
+        }
+        return true;
+    }
+
     public static boolean isbnMatch(String isbn1, String isbn2){
-        if(isbn1.length() == 13)
-            isbn1 = isbn1.substring(3, 12);
-        if(isbn2.length() == 13)
-            isbn2 = isbn2.substring(3,12);
-        return isbn1.equals(isbn2);
+        if(isbn1.length() == 13){
+            isbn1 = isbn1.substring(3);
+        }
+        if(isbn2.length() == 13) {
+            isbn2 = isbn2.substring(3);
+        }
+        // Have to ignore check digits in case we're comparing an 13 to a 10
+        return isbn1.substring(0, 8).equals(isbn2.substring(0, 8));
     }
 
     public static void fillBibTextField(TextView tv, String data){
